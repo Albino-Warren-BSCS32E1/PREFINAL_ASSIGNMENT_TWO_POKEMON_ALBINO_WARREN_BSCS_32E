2eec79393ff4693cd7c5aff50555919ebe37b34e
Warren Albino <warrenalbino277@gmail.com>
2024-05-24 23:39:24 +0800

    Add PokemonController.cs

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_ALBINO_WARREN_BSCS_32E.Controllers
+{
+    public class PokemonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
